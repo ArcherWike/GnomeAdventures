ce19b88452a9bcdefc452da4f376ecdbfb6e8317
ArcherWike <wiktoria.szczepanska.x@gmail.com>
2022-11-13 18:32:21 +0100

    Created project

---
+++ GnomeAdventures/Intermediate/Build/Win64/GnomeAdventuresEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "D:/Github/GnomeAdventures/GnomeAdventures/Intermediate/Build/Win64/GnomeAdventuresEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ GnomeAdventures/Intermediate/Build/Win64/UnrealEditor/Inc/GnomeAdventures/GnomeAdventures.init.gen.cpp
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeGnomeAdventures_init() {}
+	static FPackageRegistrationInfo Z_Registration_Info_UPackage__Script_GnomeAdventures;
+	FORCENOINLINE UPackage* Z_Construct_UPackage__Script_GnomeAdventures()
+	{
+		if (!Z_Registration_Info_UPackage__Script_GnomeAdventures.OuterSingleton)
+		{
+			static const UECodeGen_Private::FPackageParams PackageParams = {
+				"/Script/GnomeAdventures",
+				nullptr,
+				0,
+				PKG_CompiledIn | 0x00000000,
+				0x488FA7D2,
+				0x4CF7B17F,
+				METADATA_PARAMS(nullptr, 0)
+			};
+			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_GnomeAdventures.OuterSingleton, PackageParams);
+		}
+		return Z_Registration_Info_UPackage__Script_GnomeAdventures.OuterSingleton;
+	}
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_GnomeAdventures(Z_Construct_UPackage__Script_GnomeAdventures, TEXT("/Script/GnomeAdventures"), Z_Registration_Info_UPackage__Script_GnomeAdventures, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0x488FA7D2, 0x4CF7B17F));
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
